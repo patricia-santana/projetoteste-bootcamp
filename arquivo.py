d032b834c234b71d92e221df32aa9755e072bfd1
Patrícia Santana <patriciasantana.pss@gmail.com>
2021-07-12 11:51:36 -0300

    novas instruções na mensagem

--- arquivo.py
+++ arquivo.py
@@ -1,6 +1,6 @@
 idade = int(input("Qual é a sua idade"))
 
 if idade >= 18:
-    print("Você é maior de idade")
+    print("Você é maior de idade. Siga!")
 else:
-    print("Você ainda é menor de idade")
+    print("Você ainda é menor de idade. Pare!")
